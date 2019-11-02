59d6a8e88f4f9e85566f519235e495d365a109e7
EduardoLarios <eduardo.larios.fdz@outlook.com>
2019-11-01 20:41:09 -0600

    Started work on design patterns

---
+++ StrategyPatternBenchmark/IStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrategyPatternBenchmark
+{
+    // The Strategy interface declares operations common to all supported
+    // versions of some algorithm.
+    //
+    // The Context uses this interface to call the algorithm defined by Concrete
+    // Strategies.
+    public interface IStrategy
+    {
+        List<string> SortAlgorithm(List<string> data);
+    }
+}
